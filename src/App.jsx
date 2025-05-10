cf232631846de242ba81c1cdf13927008d6d4054
Oghenemaro Afenogho <maroafenogho@gmail.com>
2025-05-10 21:45:57 +0100

    children

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from './components/Card'
 import ProfileInfo from './components/ProfileInfo'
 
 
@@ -7,7 +8,12 @@ function App() {
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'column', }}>
-        <ProfileInfo />
+        <Card>
+          <ProfileInfo />
+        </Card>
+        <p style={{ margin: '30px' }}>
+          Maro is an experienced software engineer with a passion for building scalable products. He is able to take ownership of a product and build it from start to finish without missing deadlines.
+        </p>
 
       </div>
 
